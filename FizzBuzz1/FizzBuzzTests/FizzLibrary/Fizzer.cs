4077ba2b7cd2c0c453cf92efd0395cdb62f5c852
Rowland Corentin <rowland@kinesistech.co.uk>
2017-02-23 11:53:42 +0000

    REALLY high level failing tests - @wip

--- FizzBuzz1/FizzBuzzTests/FizzLibrary/Fizzer.cs
+++ FizzBuzz1/FizzBuzzTests/FizzLibrary/Fizzer.cs
@@ -8,5 +8,11 @@ namespace FizzLibrary
 {
     public class Fizzer
     {
+
+        public string GoFizz(int startNum, int endNum)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
